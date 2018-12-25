beaa77d35f8fdb3ab1e7965eb0106a2845810f17
eleele <2369745961@qq.com>
2018-12-25 21:39:35 +0800

    blabla

--- mds.py
+++ mds.py
@@ -15,27 +15,30 @@ def view(rvjs_prefix,fn,flag=1):
 	s=s.replace('<!--rvjs-->',rvjs_prefix)
 	s=s.split('<!--insert here-->')
 	s1=[s[0]]
-	f=open(fn,'r')
-	s2=f.read()
-	f.close()
-	s2=s2.replace('/a/raw/b/','')
-	s2=s2.split('---\n<!--slide-->')
-	s2=s2[1:]
-	for s3 in s2:
-		s3=s3.split('<!--config-->',maxsplit=1)
-		if (len(s3)==1): s3=['']+s3
-		attr='data-markdown'
-		try:
-			conf=json.loads(s3[0])
-			if ('html' in conf):
-				attr=attr.replace('data-markdown','')
-		except:
-			pass
-		s1.append('<section %s>'%attr)
-		s1.append(s3[1])
-		s1.append('</section>')
+	if (fn is not None):
+		f=open(fn,'r')
+		s2=f.read()
+		f.close()
+		s2=s2.replace('/a/raw/b/','')
+		s2=s2.split('---\n<!--slide-->')
+		s2=s2[1:]
+		for s3 in s2:
+			s3=s3.split('<!--config-->',maxsplit=1)
+			if (len(s3)==1): s3=['']+s3
+			attr='data-markdown'
+			try:
+				conf=json.loads(s3[0])
+				if ('html' in conf):
+					attr=attr.replace('data-markdown','')
+			except:
+				pass
+			s1.append('<section %s>'%attr)
+			s1.append(s3[1])
+			s1.append('</section>')
 	s1.append(s[1])
-	tmpf=fn+'.tmp.html'
+	if (fn is not None):
+		tmpf=fn+'.tmp.html'
+	else: tmpf='tmp.html'
 	f=open(tmpf,'w')
 	f.write('\n'.join(s1))
 	f.close()
